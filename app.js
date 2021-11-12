ef8a3468433051eb594dc8361b3232cc7c91c1b2
yusuf kaygin <yusuf.two000@gmail.com>
2021-11-12 17:45:07 +0300

    view engine uyarlamasi

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const exphbs = require('express-handlebars');
 const port = 3000;
 const hostName = '127.0.0.1';   
 
 app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'site/index.html'));
+    res.render('site/index');
 })
 
-app.get('/about.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'site/about.html'));
+app.get('/about', (req, res) => {
+    res.render('site/about');
 })
 
-app.get('/blog.html', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'site/blog.html'));
+app.get('/blog', (req, res) => {
+    res.render('site/blog');
 })
+
 
 app.listen(port, hostName, () => {
-    console.log('Proje ayağa kalktı.');
+    console.log('Server on');
 })
