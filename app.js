681dc302998dc18979b0625fdb522afe36dd0414
yusuf kaygin <yusuf.two000@gmail.com>
2021-11-12 03:54:22 +0300

    proje dosyalarinin eklenmesi

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const app = express();
 const port = 3000;
 const hostName = '127.0.0.1';   
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.sendFile(path.resolve(__dirname, 'site/index.html'));
+})
+
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'site/about.html'));
+})
+
+app.get('/blog.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'site/blog.html'));
 })
 
 app.listen(port, hostName, () => {
